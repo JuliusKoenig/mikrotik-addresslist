60aba81dfb5c37f512e0ecebf8e156259f095a53
Julius Koenig <julius@koenig-site.de>
2025-08-14 15:43:20 +0200

    initialize project structure with .gitignore, LICENSE, README, and configuration files

---
+++ src/mikrotik_addresslist/__init__.py
@@ -0,0 +1,8 @@
+__title__ = "mikrotik-addresslist"
+__description__ = ""  # ToDo: Add a short description of the package
+__version__ = "0.1.0"
+__author__ = "Julius Koenig"
+__author_email__ = "info@bastelquartier.de"
+__license__ = "GPL-3.0"
+__license_url__ = "https://www.gnu.org/licenses/gpl-3.0.md"
+__terms_of_service__ = "https://bastelquartier.de/terms-of-service"
