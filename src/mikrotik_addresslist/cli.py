43389e12230320a10fb3fa5429d626901a1a935b
Julius Koenig <julius@koenig-site.de>
2025-08-15 14:43:57 +0200

    enhance CLI for generating Mikrotik RouterOS scripts with additional options and logging capabilities

--- src/mikrotik_addresslist/cli.py
+++ src/mikrotik_addresslist/cli.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from typing import NoReturn
 
 import typer
-from mikrotik_addresslist.generate_script import generate_script
+from mikrotik_addresslist.generate_script import generate_script, ScriptLogLevel
 from wiederverwendbar.typer import Typer
 from wiederverwendbar.logger import LoggerSingleton
 
@@ -21,25 +21,69 @@ cli_app = Typer(settings=settings)
 
 @cli_app.command(name="generate-script", help=f"Generate a Mikrotik RouterOS script from local file or URL.")
 def generate_script_command(file: Path | None = typer.Option(None,
-                                                      "-l", "--local-file",
-                                                      help="Path to the local file containing the IP addresses.",
-                                                      exists=True),
+                                                             "-f", "--local-file",
+                                                             help="Path to the local file containing the IP addresses.",
+                                                             exists=True),
                             url: str | None = typer.Option(None,
-                                                    "-u", "--url-file",
-                                                    help="URL to the file containing the IP addresses."),
+                                                           "-u", "--url-file",
+                                                           help="URL to the file containing the IP addresses."),
+                            list_name: str = typer.Option(...,
+                                                          "-n", "--list-name",
+                                                          help="Name of the address list to be created in RouterOS."),
                             output_file: Path = typer.Option(None,
                                                              "-o", "--output-file",
                                                              help="Path to the output file where the generated script will be saved."),
+                            header: list[str] | None = typer.Option(None,
+                                                                    "-h", "--header",
+                                                                    help="Header comment for the generated script."),
+                            comment: str | None = typer.Option(None,
+                                                               "-c", "--comment",
+                                                               help="Comment for the address list entries."),
+                            timeout: int | None = typer.Option(None,
+                                                               "-t", "--timeout",
+                                                               help="Timeout for dynamic address list entries in seconds."),
+                            script_log_level: ScriptLogLevel = typer.Option(ScriptLogLevel.debug,
+                                                                            "-l", "--log-level",
+                                                                            help="Log level for the generated script."),
+                            no_catch_errors: bool = typer.Option(False,
+                                                                 "-nce", "--no-catch-errors",
+                                                                 help="Disable error catching in the generated script."),
+                            no_ipv4: bool = typer.Option(False,
+                                                         "-n4", "--no-ipv4",
+                                                         help="Disable IPv4 addresses in the generated script."),
+                            no_ipv6: bool = typer.Option(False,
+                                                         "-n6", "--no-ipv6",
+                                                         help="Disable IPv6 addresses in the generated script."),
+
                             force: bool = typer.Option(False,
-                                                       "-f", "--force",
-                                                       help="Force overwrite the output file if it already exists.")) -> NoReturn:
-    cli_app.console.print(f"Generating script from {'local file' if file else 'URL'} '{file or url}' ...")
+                                                       "-F", "--force",
+                                                       help="Force overwrite the output file if it already exists."),
+                            dynamic: bool = typer.Option(False,
+                                                         "-d", "--dynamic",
+                                                         help="Create dynamic address list entries."),
+                            disabled: bool = typer.Option(False,
+                                                          "-D", "--disabled",
+                                                          help="Create disabled address list entries.")) -> NoReturn:
 
     try:
-        result = generate_script(file=file, url=url, force=force)
+        output = generate_script(file=file,
+                                 url=url,
+                                 list_name=list_name,
+                                 header=header,
+                                 comment=comment,
+                                 timeout=timeout,
+                                 script_log_level=script_log_level,
+                                 no_catch_errors=no_catch_errors,
+                                 no_ipv4=no_ipv4,
+                                 no_ipv6=no_ipv6,
+                                 force=force,
+                                 dynamic=dynamic,
+                                 disabled=disabled)
     except Exception as e:
-        cli_app.console.print(f"[red]Error generating script: {e}[/red]")
+        cli_app.console.print(f"Error generating script: {e}", file=sys.stderr)
         sys.exit(1)
-
-    cli_app.console.print(f"Script generated successfully:\n{result}")
+    if output_file:
+        ...
+    else:
+        cli_app.console.print(output)
     sys.exit(0)
--- src/mikrotik_addresslist/settings.py
+++ src/mikrotik_addresslist/settings.py
@@ -1,6 +1,6 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings
-from wiederverwendbar.logger import LoggerSettings, LogLevels
+from wiederverwendbar.logger import LoggerSettings
 from wiederverwendbar.typer import TyperSettings
 
 from mikrotik_addresslist import __title__, __description__, __author__, __author_email__, __version__, __license__, __license_url__, __terms_of_service__, __env_prefix__
@@ -12,6 +12,10 @@ class Settings(BaseSettings, LoggerSettings, TyperSettings):
         "case_sensitive": False
     }
 
+    datetime_format: str = Field(default="%d.%m.%Y - %H:%M:%S",
+                                 title="Date and Time Format",
+                                 description="Format for date and time in the generated script.")
+
 
 settings = Settings(branding_title=__title__,
                     branding_description=__description__,
@@ -20,5 +24,4 @@ settings = Settings(branding_title=__title__,
                     branding_author_email=__author_email__,
                     branding_license=__license__,
                     branding_license_url=__license_url__,
-                    branding_terms_of_service=__terms_of_service__,
-                    log_level=LogLevels.DEBUG)  # ToDo: Change to LogLevels.INFO in production
+                    branding_terms_of_service=__terms_of_service__)
